4eec94ec7483a4fd1550f65adb60faff87057449
ankur srivastava <careermailankur@gmail.com>
2021-07-01 17:17:15 +0530

    changes

--- backend/server.js
+++ backend/server.js
@@ -12,9 +12,9 @@ process.on('uncaughtException', err => {
 })
 
 // Setting up config file
-if (process.env.NODE_ENV !== 'PRODUCTION') require('dotenv').config({ path: 'backend/config/config.env' })
+// if (process.env.NODE_ENV !== 'PRODUCTION') require('dotenv').config({ path: 'backend/config/config.env' })
 
-// dotenv.config({ path: 'backend/config/config.env' })
+dotenv.config({ path: 'backend/config/config.env' })
 
 
 // Connecting to database
